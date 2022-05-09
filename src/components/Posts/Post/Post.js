460338285c45b63c8300753e6299032f08d3a483
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2022-05-09 14:35:02 +0500

    folder structue setup

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,10 @@
+import React from "react";
+import useStyles from "./styles";
+
+const Post = () => {
+  const classes = useStyles();
+
+  return <h1>Sinle Post Component</h1>;
+};
+
+export default Post;
